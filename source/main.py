23f460ae9abfe7ed94e2868f23963b2501be1a7e
Muhammad Mudasir Khan <m.mudassirkhan@gmail.com>
2020-12-22 03:46:07 +0100

    version endpoint

--- source/main.py
+++ source/main.py
@@ -1,7 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
+import subprocess
 import re
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welocme to The homepage!</h1>"
 
 
 @app.route('/helloworld')
@@ -12,6 +18,17 @@ def hello_world():
         return 'Hello' + ' ' + greetings
     else:
         return "Hello Stranger"
+
+
+@app.route("/versionz")
+def versionz():
+    label = subprocess.check_output(["git", "rev-parse", "--show-toplevel", "HEAD"]).strip()
+    return label
+
+
+with app.test_request_context():
+    print(url_for('hello_world'))
+    print(url_for('versionz'))
 
 
 if __name__ == '__main__':
