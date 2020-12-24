b1adcaaf1aaf51ffb5fb91efa9463607c9d6ef3d
Muhammad Mudasir Khan <m.mudassirkhan@gmail.com>
2020-12-24 19:19:22 +0100

    hello_service

---
+++ source/hello_service.py
@@ -0,0 +1,12 @@
+import re
+
+
+class HelloService:
+
+    def say_hello(self, user_name):
+        if user_name is None:
+            return "Hello Stranger"
+
+        else:
+            greetings = re.sub(r'((?<=[a-z])[A-Z]|[A-Z](?=[a-z]))', r' \1', user_name).strip()
+            return 'Hello' + ' ' + greetings
