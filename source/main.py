3fe36b0b2a259b0f868e95c41e9195320b25fa81
Muhammad Mudasir Khan <m.mudassirkhan@gmail.com>
2020-12-24 15:38:18 +0100

    version function

--- source/main.py
+++ source/main.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, url_for
+from flask import Flask, request
 import logging
-import subprocess
-import sys
+import requests
 import re
-
 
 logging.basicConfig(
     filename='service.log',
@@ -14,7 +12,6 @@ logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
-
 
 app = Flask(__name__)
 
@@ -36,13 +33,20 @@ def hello_world():
 
 @app.route("/versionz")
 def versionz():
-    label = subprocess.check_output(["git", "rev-parse", "--show-toplevel", "HEAD"]).strip()
-    return label
+    api_url = "https://api.github.com/repos"
+    owner = "Mudassir86"
+    repo = "HttpServiceApp"
+    base_url = api_url + "/" + owner + "/" + repo
 
+    response_api = requests.get(base_url)
+    response_api_json = response_api.json()
 
-with app.test_request_context():
-    print(url_for('hello_world'))
-    print(url_for('versionz'))
+    branch = 'master'
+    commit_api_url = base_url + '/commits' + '/' + branch
+    response_commit = requests.get(commit_api_url)
+    response_commit_json = response_commit.json()
+
+    return {'name': response_api_json['name'], 'last_commit_hash': response_commit_json['sha']}
 
 
 if __name__ == '__main__':
